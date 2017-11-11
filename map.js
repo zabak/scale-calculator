48ec2dc7aae1dea51a55ba7f9944ad410a7cbb24
Erich Duda <dudaerich@gmail.com>
2017-11-11 19:19:23 +0100

    Initial commit

---
+++ map.js
@@ -0,0 +1,202 @@
+var raster = new ol.layer.Tile({
+  source: new ol.source.OSM()
+});
+
+var source = new ol.source.Vector();
+
+var vector = new ol.layer.Vector({
+  source: source,
+  style: new ol.style.Style({
+    fill: new ol.style.Fill({
+      color: 'rgba(255, 255, 255, 0.2)'
+    }),
+    stroke: new ol.style.Stroke({
+      color: '#ffcc33',
+      width: 2
+    }),
+    image: new ol.style.Circle({
+      radius: 7,
+      fill: new ol.style.Fill({
+        color: '#ffcc33'
+      })
+    })
+  })
+});
+
+
+/**
+ * Currently drawn feature.
+ * @type {ol.Feature}
+ */
+var sketch;
+
+
+/**
+ * The help tooltip element.
+ * @type {Element}
+ */
+var helpTooltipElement;
+
+
+/**
+ * Overlay to show the help messages.
+ * @type {ol.Overlay}
+ */
+var helpTooltip;
+
+
+/**
+ * The measure tooltip element.
+ * @type {Element}
+ */
+var measureTooltipElement;
+
+
+/**
+ * Overlay to show the measurement.
+ * @type {ol.Overlay}
+ */
+var measureTooltip;
+
+var mapElement = document.getElementById('map');
+
+
+var map = new ol.Map({
+  layers: [raster, vector],
+  target: 'map',
+  view: new ol.View({
+    center: [0, 0],
+    zoom: 2
+  })
+});
+
+
+var draw; // global so we can remove it later
+
+var valueUnitInput = $('#input-value-unit');
+var systemInput = $('#input-system');
+var unitInput = $('#input-unit');
+
+/**
+ * Format length output.
+ * @param {ol.geom.LineString} line The line.
+ * @return {string} The formatted length.
+ */
+var formatLength = function(line) {
+  var length = ol.Sphere.getLength(line);
+
+  systemInput.val(systemForMap);
+  systemInput.trigger('change');
+  unitInput.val(ratioForMap);
+  unitInput.trigger('change');
+  valueUnitInput.val(Math.round(length / 1000 * 100) / 100);
+  valueUnitInput.trigger('keyup');
+  var output;
+  if (length > 100) {
+    output = (Math.round(length / 1000 * 100) / 100) +
+        ' ' + 'km';
+  } else {
+    output = (Math.round(length * 100) / 100) +
+        ' ' + 'm';
+  }
+  return output;
+};
+
+
+function addInteraction() {
+  draw = new ol.interaction.Draw({
+    source: source,
+    type: /** @type {ol.geom.GeometryType} */ ('LineString'),
+    maxPoints: 2,
+    style: new ol.style.Style({
+      fill: new ol.style.Fill({
+        color: 'rgba(255, 255, 255, 0.2)'
+      }),
+      stroke: new ol.style.Stroke({
+        color: 'rgba(0, 0, 0, 0.5)',
+        lineDash: [10, 10],
+        width: 2
+      }),
+      image: new ol.style.Circle({
+        radius: 5,
+        stroke: new ol.style.Stroke({
+          color: 'rgba(0, 0, 0, 0.7)'
+        }),
+        fill: new ol.style.Fill({
+          color: 'rgba(255, 255, 255, 0.2)'
+        })
+      })
+    })
+  });
+  map.addInteraction(draw);
+
+  createMeasureTooltip();
+  createHelpTooltip();
+
+  var listener;
+  draw.on('drawstart',
+      function(evt) {
+        source.clear(true);
+        createMeasureTooltip();
+        // set sketch
+        sketch = evt.feature;
+
+        /** @type {ol.Coordinate|undefined} */
+        var tooltipCoord = evt.coordinate;
+
+        listener = sketch.getGeometry().on('change', function(evt) {
+          var geom = evt.target;
+          var output = formatLength(geom);
+          tooltipCoord = geom.getLastCoordinate();
+          measureTooltipElement.innerHTML = output;
+          measureTooltip.setPosition(tooltipCoord);
+        });
+      }, this);
+
+  draw.on('drawend',
+      function() {
+        measureTooltipElement.className = 'tooltip tooltip-static';
+        measureTooltip.setOffset([0, -7]);
+        // unset sketch
+        sketch = null;
+        ol.Observable.unByKey(listener);
+      }, this);
+}
+
+
+/**
+ * Creates a new help tooltip
+ */
+function createHelpTooltip() {
+  if (helpTooltipElement) {
+    helpTooltipElement.parentNode.removeChild(helpTooltipElement);
+  }
+  helpTooltipElement = document.createElement('div');
+  helpTooltipElement.className = 'tooltip hidden';
+  helpTooltip = new ol.Overlay({
+    element: helpTooltipElement,
+    offset: [15, 0],
+    positioning: 'center-left'
+  });
+  map.addOverlay(helpTooltip);
+}
+
+
+/**
+ * Creates a new measure tooltip
+ */
+function createMeasureTooltip() {
+  if (measureTooltipElement) {
+    measureTooltipElement.parentNode.removeChild(measureTooltipElement);
+  }
+  measureTooltipElement = document.createElement('div');
+  measureTooltipElement.className = 'tooltip tooltip-measure';
+  measureTooltip = new ol.Overlay({
+    element: measureTooltipElement,
+    offset: [0, -15],
+    positioning: 'bottom-center'
+  });
+  map.addOverlay(measureTooltip);
+}
+
+addInteraction();
---
+++ systems.js
@@ -0,0 +1,233 @@
+var systems = {
+  "česká (do 1765)": [
+    {
+      "name": {
+        "cs": "palec",
+        "de": "böhmischer Zoll"
+      },
+      "value": 24.7
+    }, {
+      "name": {
+        "cs": "stopa",
+        "de": "böhmischer Schuh"
+      },
+      "value": 295.7
+    }, {
+      "name": {
+        "cs": "loket",
+        "de": "bömische Elle"
+      },
+      "value": 591.4
+    }, {
+      "name": {
+        "cs": "sáh",
+        "de": "böhmischer Klafter"
+      },
+      "value": 1774.2
+    }, {
+      "name": {
+        "cs": "provazec",
+        "de": "böhmische Seile"
+      },
+      "value": 30752.8
+    }, {
+      "name": {
+        "cs": "míle",
+        "de": "böhmische Meile"
+      },
+      "value": 11224772
+    }
+  ],
+  "dolnorakouská": [
+    {
+      "name": {
+        "cs": "palec dolnorakouský",
+        "de": "niederösterreichischer Zoll"
+      },
+      "value": 26.3
+    }, {
+      "name": {
+        "cs": "stopa dolnorakouská",
+        "de": "niederösterreichischer Schuh"
+      },
+      "value": 316
+    }, {
+      "name": {
+        "cs": "loket dolnorakouský",
+        "de": "niederösterreichische Elle"
+      },
+      "value": 777
+    }, {
+      "name": {
+        "cs": "sáh dolnorakouský",
+        "de": "niederösterreichischer Klafter"
+      },
+      "value": 1896
+    }, {
+      "name": {
+        "cs": "míle dolnorakouská",
+        "de": "niederösterreichische Meile"
+      },
+      "value": 7586000
+    }
+  ],
+  "moravská (do 1758)": [
+    {
+      "name": {
+        "cs": "palec moravský",
+        "de": "mährischer Zoll"
+      },
+      "value": 24.75
+    }, {
+      "name": {
+        "cs": "stopa moravská",
+        "de": "mährischer Schuh"
+      },
+      "value": 295.9
+    }, {
+      "name": {
+        "cs": "loket moravský",
+        "de": "mährische Elle"
+      },
+      "value": 788.91
+    }, {
+      "name": {
+        "cs": "sáh moravský",
+        "de": "mährischer Klafter"
+      },
+      "value": 1777.5
+    }
+  ],
+  "slezská (do 1750)": [
+    {
+      "name": {
+        "cs": "palec slezský",
+        "de": "schlesischer Zoll"
+      },
+      "value": 24
+    }, {
+      "name": {
+        "cs": "stopa slezská",
+        "de": "schlesischer Schuh"
+      },
+      "value": 289.6
+    }, {
+      "name": {
+        "cs": "loket slezský",
+        "de": "schlesische Elle"
+      },
+      "value": 574.61
+    }, {
+      "name": {
+        "cs": "sáh slezský",
+        "de": "schlesischer Klafter"
+      },
+      "value": 1735
+    }, {
+      "name": {
+        "cs": "provazec slezský",
+        "de": "schlesische Seile"
+      },
+      "value": 432.75
+    }, {
+      "name": {
+        "cs": "míle slezská",
+        "de": "schlesische Meile"
+      },
+      "value": 6491000
+    }
+  ],
+  "pruská": [
+    {
+      "name": {
+        "cs": "palec pruský",
+        "de": "preußischer Zoll"
+      },
+      "value": 26
+    }, {
+      "name": {
+        "cs": "stopa pruská",
+        "de": "preußischer Schuh"
+      },
+      "value": 313.85
+    }, {
+      "name": {
+        "cs": "loket pruský",
+        "de": "preußische Elle"
+      },
+      "value": 666.9
+    }, {
+      "name": {
+        "cs": "sáh pruský",
+        "de": "preußischer Klafter"
+      },
+      "value": 1883.1
+    }, {
+      "name": {
+        "cs": "míle pruská",
+        "de": "preußische Meile"
+      },
+      "value": 7533000
+    }
+  ],
+  "metrická": [
+    {
+      "name": {
+        "cs": "centimetr",
+        "de": "Zentimeter"
+      },
+      "value": 10
+    }, {
+      "name": {
+        "cs": "metr",
+        "de": "Meter"
+      },
+      "value": 1000
+    }, {
+      "name": {
+        "cs": "kilometr",
+        "de": "Kilometer"
+      },
+      "value": 1000000
+    }
+  ],
+  "slovenská-uherská (18. století)": [
+    {
+      "name": {
+        "cs": "loket slovenský",
+        "de": "slowakische Elle"
+      },
+      "value": 360
+    }, {
+      "name": {
+        "cs": "královský sáh slovenský",
+        "de": "slowakischer königlicher Klafter"
+      },
+      "value": 1800
+    }
+  ],
+  "slovenská-uherská (19. století)": [
+    {
+      "name": {
+        "cs": "loket slovenský",
+        "de": "slowakische Elle"
+      },
+      "value": 737.53
+    }, {
+      "name": {
+        "cs": "sáh slovenský",
+        "de": "slowakischer Klafter"
+      },
+      "value": 1896.48
+    }, {
+      "name": {
+        "cs": "míle uherská",
+        "de": "ungarische Meile"
+      },
+      "value": 8353600
+    }
+  ]
+}
+
+var systemForMap = 'metrická';
+var ratioForMap = 1000000;
